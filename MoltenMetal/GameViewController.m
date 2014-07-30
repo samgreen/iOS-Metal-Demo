469a4caab253a92a973622e59dab36030d30d0f9
samgreen <greensamuelm@gmail.com>
2014-07-29 22:02:49 -0700

    Disabled status bar.

--- MoltenMetal/GameViewController.m
+++ MoltenMetal/GameViewController.m
@@ -118,6 +118,10 @@ typedef struct
     
 }
 
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
